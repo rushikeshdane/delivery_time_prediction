2ccc899d7180d86a3d22158f1c0a3b2730b2ef89
Rushikesh Dane <78649021+rushikeshdane@users.noreply.github.com>
2025-01-29 13:52:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,4 +42,4 @@ def predict():
         return render_template('index.html', prediction_text=f'Error: {str(e)}')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(hpst ="0.0.0.0" ,debug=True)
